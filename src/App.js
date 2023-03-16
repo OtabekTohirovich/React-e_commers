6fd6277283a9268ced6a8abd046410a72733e4d1
OtabekTohirovich <tohirovo630@gmail.com>
2023-03-16 22:19:23 +0500

    Progress structure react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      {/* <Navbar /> */}
+      {/* <Routes> */}
+        {/* <Route path="/" /> */}
+        {/* <Route path="/channel/:id" element={<Chanel />} /> */}
+        {/* <Route path="/vedio/:id" element={<Vediodetails />} /> */}
+        {/* <Route path="/search/:id" element={<Search />} /> */}
+      {/* </Routes> */}
+    </Box>
   );
 }
 
